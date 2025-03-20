e030ff090b687aabfdeb19f60ba2425b6f35e14a
350lagg <genshin1mp@mail.ru>
2025-03-20 22:52:33 +0300

    Созданы объявления функций класса GameField

--- tic_tac_toe/functions.h
+++ tic_tac_toe/functions.h
@@ -2,5 +2,11 @@
 
 class GameField
 {
-
+private:
+    char board[3][3];
+public:
+    GameField();
+    GameField(char b[3][3]);
+    char get(int i, int j) const;
+    void set(int i, int j, char value);
 };
